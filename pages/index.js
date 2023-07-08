afdb3792d87ce312a90528b24f314d1d3d01ef78
GOUTHAM G <chintugoutham2@gmail.com>
2023-07-08 23:46:49 +0530

    Navbar check

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-
+import ButtonAppBar from './NavBar'
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div >
       <Head>
         <title>Hello ! This is Goutham</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Hello React ! </div>
+      <ButtonAppBar/>
+      
     </div>
   )
 }
